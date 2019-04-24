05ea425524efeac2d19a3d50d34218e9081a03c1
nsaip <nadiasaip@gmail.com>
2019-04-24 13:41:29 -0500

    ~ added instead of root dir

--- awskeypair.tf
+++ awskeypair.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key1"
-  public_key = "${file("/root/.ssh/id_rsa.pub")}"
+  public_key = "${file("/~/.ssh/id_rsa.pub")}"
  
 }
 
@@ -9,5 +9,5 @@ resource "aws_key_pair" "deployer" {
 resource "aws_key_pair" "ec2-ssh-key2" {
   key_name = "ec2-ssh-key2"
 
-  public_key = "${file("/root/.ssh/id_rsa.pub")}"
+  public_key = "${file("/~/.ssh/id_rsa.pub")}"
 }
